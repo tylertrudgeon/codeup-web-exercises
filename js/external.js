54e0c63241e0eecd8017c84f7968a3177cec8855
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-03-16 17:01:00 -0600

    started JavaScript with html exercise.

---
+++ js/external.js
@@ -0,0 +1,17 @@
+console.log('Hello from external JavaScript');
+
+alert('Welcome to my Website!');
+
+var favoriteColor = prompt('What is your favorite color?');
+alert(favoriteColor + ' is my favorite color too!');
+
+var littleMermaid = prompt('How many days will you rent The Little Mermaid?');
+
+var brotherBear = prompt('How many days will you rent Brother bear?');
+
+var hercules = prompt('How many days will you rent Hercules?');
+
+var totalCost = (littleMermaid * 3.00) + (brotherBear * 3.00) + (hercules * 3.00)
+
+alert(totalCost + (' dollars is your total cost.'));
+
