cdbcf0ca82c2f5d46231a567d0dd83ac9f6ea1db
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-03-17 11:08:56 -0600

    finished JavaScript exercises

--- js/external.js
+++ js/external.js
@@ -7,11 +7,53 @@ alert(favoriteColor + ' is my favorite color too!');
 
 var littleMermaid = prompt('How many days will you rent The Little Mermaid?');
 
-var brotherBear = prompt('How many days will you rent Brother bear?');
+var brotherBear = prompt('How many days will you rent Brother Bear?');
 
 var hercules = prompt('How many days will you rent Hercules?');
 
-var totalCost = (littleMermaid * 3.00) + (brotherBear * 3.00) + (hercules * 3.00)
+var totalCost = (littleMermaid * 3.00) + (brotherBear * 3.00) + (hercules * 3.00);
 
 alert(totalCost + (' dollars is your total cost.'));
+
+
+
+var googlePay = prompt('How much is your hourly pay for Google?');
+
+var amazonPay = prompt('How much is your hourly pay for Amazon?');
+
+var facebookPay = prompt('How much is your hourly pay for Facebook?');
+
+var googleHours = prompt('How many hours did you work for Google?');
+
+var amazonHours = prompt("How many hours did you work for Amazon?");
+
+var facebookHours = prompt("How many hours did you work for Facebook?");
+
+var totalPay = (googlePay * googleHours) + (amazonPay * amazonHours) + (facebookPay * facebookHours);
+
+alert(totalPay + (' dollars is how much you will be paid.'));
+
+
+
+var classIsFull = false;
+
+var classScheduleConflict = false;
+
+var canAttendClass = (!classIsFull && !classScheduleConflict);
+
+alert('Student can attend class ' + canAttendClass);
+
+
+
+var numberOfItems = prompt('How many items will you be buying?');
+
+numberOfItems = parseInt(numberOfItems);
+
+var isOfferExpired = false;
+
+var isCustomerPremium = false;
+
+var isOfferValid = (isCustomerPremium && !isOfferExpired) || (numberOfItems >= 2 && !isOfferExpired);
+
+alert('Offer is valid ' + isOfferValid);
 
