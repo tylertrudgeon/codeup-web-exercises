1e581ae39bf3b83859a38b8c9208d8f9c43855fd
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-03-25 13:54:56 -0500

    finalized updates to javascript loops exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,18 +1,27 @@
 
 
-var userEntry = parseInt(prompt('Please enter and odd number between 1 and 50.'));
+var userNumber = 0;
 
-for ( var i = 1; i < 50; i++){
-    if (userEntry % 2 === 0 || userEntry > 50) {
-        userEntry= prompt('That was not a valid number, please try again.');
-        console.log('The number to skip is: ' + userEntry);
+do {
+    userNumber = Number(prompt('Please enter an odd number between 1 and 50.'));
+    if (userNumber < 1 || userNumber > 50) {
+        alert(userNumber + ' is not between 1 and 50.');
+    }else if (userNumber % 2 === 0) {
+        alert(userNumber + 'is not odd.');
+    }else if (isNaN(userNumber)) {
+        alert(userNumber + ' is not a number.');
+    } else {
+        alert('You have entered a valid number.');
         break;
     }
-    else if(i % 2 !== 0) {
-        console.log('Here is an odd number: ' + i)
-    }
-    else if (i === userEntry) {
+} while(true);
+
+console.log('Number to skip is: ' + userNumber);
+
+for (var i = 1; i <= 50; i += 2) {
+    if (i === userNumber) {
+        console.log('Yikes! Skipping number: ' + userNumber);
         continue;
     }
-    console.log('Yikes! Skipping number: ' + userEntry);
+    console.log('Here is an odd number: ' + i);
 }
